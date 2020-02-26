c2df157bf846803bb08f821612ea9b451e59809a
DunkRing <Chris31@live.dk>
2020-02-26 17:04:17 +0100

    Name fix
    ocd

--- functions.sql
+++ functions.sql
@@ -6,20 +6,20 @@
 CREATE OR REPLACE FUNCTION LoanABook(ISBNNumberValue integer, CPRNumbmerValue varchar(10), LoanPeriodStartValue date, LoanPeriodEndValue date)
 RETURNS void AS $$
 DECLARE
-  copiesOfSelectedBook int 			:= (SELECT bookamount FROM status WHERE isbn = ISBNNumberValue);  	--Fecthing how many copies of a book
-  amountRentedOut int 				:= (SELECT bookrented FROM status WHERE isbn = ISBNNumberValue);  	--Fecthing how many book of one type is rented out
-  typeOfLoaner varchar(100) 			:= (SELECT typeof FROM loaner WHERE cprnummer = CPRNumbmerValue); 	--Fecthing the loaners type
-  loanersRentedBookAmount int 			:= (SELECT COUNT(*) FROM borrowed where cprnummer = CPRNumbmerValue); 	--Fecthing amount of books the loaner has rented
+  copiesOfSelectedBook int 			:= (SELECT BookAmount FROM Status WHERE ISBN = ISBNNumberValue);  	--Fecthing how many copies of a book
+  amountRentedOut int 				:= (SELECT BookRented FROM Status WHERE ISBN = ISBNNumberValue);  	--Fecthing how many book of one type is rented out
+  typeOfLoaner varchar(100) 			:= (SELECT TypeOf FROM Loaner WHERE CPRNumber = CPRNumbmerValue); 	--Fecthing the loaners type
+  loanersRentedBookAmount int 			:= (SELECT COUNT(*) FROM Borrowed WHERE CPRNumber = CPRNumbmerValue); 	--Fecthing amount of books the loaner has rented
 BEGIN
   IF copiesOfSelectedBook > amountRentedOut THEN
 	IF typeOfLoaner = 'Student' THEN
 		IF loanersRentedBookAmount < 3 THEN
 			RAISE NOTICE 'Book available, renting one out..';
 			--Creates a new borrow
-			INSERT INTO borrowed (isbn, cprnummer, loanperiodstart, loanperiodend)
+			INSERT INTO Borrowed (ISBN, CPRNumber, LoanPeriodStart, LoanPeriodEnd)
 			VALUES (ISBNNumberValue, CPRNumbmerValue, LoanPeriodStartValue, LoanPeriodEndValue);
 			--Updates the status
-			UPDATE status SET bookrented = bookrented+1 WHERE isbn=ISBNNumberValue;
+			UPDATE Status SET BookRented = BookRented + 1 WHERE ISBN = ISBNNumberValue;
 		ELSE
 			RAISE NOTICE 'STUDENT - du kan ikke låne flere bøger';
 		END IF;
@@ -28,10 +28,10 @@ BEGIN
 		IF loanersRentedBookAmount < 2 THEN
 			RAISE NOTICE 'Book available, renting one out..';
 			--Creates a new borrow
-			INSERT INTO borrowed (isbn, cprnummer, loanperiodstart, loanperiodend)
+			INSERT INTO Borrowed (ISBN, CPRNumber, LoanPeriodStart, LoanPeriodEnd)
 			VALUES (ISBNNumberValue, CPRNumbmerValue, LoanPeriodStartValue, LoanPeriodEndValue);
 			--Updates the status
-			UPDATE status SET bookrented = bookrented+1 WHERE isbn=ISBNNumberValue;
+			UPDATE Status SET BookRented = BookRented + 1 WHERE ISBN = ISBNNumberValue;
 		ELSE
 			RAISE NOTICE 'TEACHER - du kan ikke låne flere bøger';
 		END IF;
@@ -40,10 +40,10 @@ BEGIN
 		IF loanersRentedBookAmount < 1 THEN
 			RAISE NOTICE 'Book available, renting one out..';
 			--Creates a new borrow
-			INSERT INTO borrowed (isbn, cprnummer, loanperiodstart, loanperiodend)
+			INSERT INTO Borrowed (ISBN, CPRNumber, LoanPeriodStart, LoanPeriodEnd)
 			VALUES (ISBNNumberValue, CPRNumbmerValue, LoanPeriodStartValue, LoanPeriodEndValue);
 			--Updates the status
-			UPDATE status SET bookrented = bookrented+1 WHERE isbn=ISBNNumberValue;
+			UPDATE Status SET BookRented = BookRented + 1 WHERE ISBN = ISBNNumberValue;
 		ELSE
 			RAISE NOTICE 'GUEST - du kan ikke låne flere bøger';
 		END IF;
@@ -58,19 +58,19 @@ LANGUAGE PLPGSQL;
 
 
 -- TEST FUNKTIONALITET AND SHORTCUTS --
-INSERT INTO borrowed (isbn, CPRNummer, LoanPeriodStart, LoanPeriodEnd)values (22222, 1234567890, '2020-02-25', '2020-02-28');
+INSERT INTO Borrowed (ISBN, CPRNumber, LoanPeriodStart, LoanPeriodEnd) VALUES (22222, 1234567890, '2020-02-25', '2020-02-28');
 
-INSERT INTO status (isbn, bookamount, bookrented) values (11111, 2, 0);
-INSERT INTO status (isbn, bookamount, bookrented) values (88888, 5, 0);
+INSERT INTO Status (ISBN, BookAmount, BookRented) VALUES (11111, 2, 0);
+INSERT INTO Status (ISBN, BookAmount, BookRented) VALUES (88888, 5, 0);
 
 -- Function call
-select LoanABook(11111, '1234561891', '2020-02-15', '2020-02-28');
+SELECT LoanABook(11111, '1234561891', '2020-02-15', '2020-02-28');
 
 -- Test helpers
-delete from borrowed;
-update status set bookrented = 0 where isbn=88888;
-select * from borrowed;
-select * from status;
+DELETE FROM Borrowed;
+UPDATE Status SET BookRented = 0 WHERE ISBN = 88888;
+SELECT * FROM Borrowed;
+SELECT * FROM Status;
 
 
 
@@ -79,15 +79,15 @@ select * from status;
 CREATE OR REPLACE FUNCTION CheckBookIsAvail(ISBNNumber integer)
 RETURNS int AS $$
 DECLARE
-	a int := (SELECT bookamount FROM status WHERE isbn = 44444);  --Fecthing how many copies of a book
-  	b int := (SELECT bookrented FROM status WHERE isbn = 44444);  --Fecthing how many book a one type is rented out
+	copiesOfSelectedBook int	:= (SELECT BookAmount FROM Status WHERE ISBN = 44444);  --Fecthing how many copies of a book
+  	amountRentedOut int 		:= (SELECT BookRented FROM Status WHERE ISBN = 44444);  --Fecthing how many book a one type is rented out
 BEGIN
-	RETURN a-b;
+	RETURN copiesOfSelectedBook - amountRentedOut;
 END; $$
 LANGUAGE PLPGSQL;
 
 -- Test Function
-Select CheckBookIsAvail(44444);
+SELECT CheckBookIsAvail(44444);
 
 
 
@@ -95,10 +95,10 @@ Select CheckBookIsAvail(44444);
 CREATE OR REPLACE FUNCTION CheckMostPopularTitleStudents()
 RETURNS varChar(50) AS $$
 DECLARE
-	isbnOfMostPopularBook int := (SELECT isbn FROM(select isbn, count(*) from borrowed inner JOIN loaner ON loaner.cprnummer = borrowed.cprnummer where loaner.typeof = 'Student' GROUP BY isbn ORDER BY count DESC limit 1) a);
-	bookTitle varchar(100) := (select book.title from book where isbn=isbnOfMostPopularBook);
+	isbnOfMostPopularBook int := (SELECT ISBN FROM (SELECT ISBN, COUNT(*) FROM Borrowed INNER JOIN Loaner ON Loaner.CPRNumber = Borrowed.CPRNumber WHERE Loaner.TypeOf = 'Student' GROUP BY ISBN ORDER BY COUNT DESC LIMIT 1) a);
+	bookTitle varchar(100) := (SELECT Book.Title FROM Book WHERE ISBN = isbnOfMostPopularBook);
 BEGIN
-	return bookTitle;
+	RETURN bookTitle;
 END; $$
 LANGUAGE PLPGSQL;
 
@@ -110,9 +110,9 @@ select CheckMostPopularTitleStudents();
 
 ------ The library wants to keep track of each copy of each book’s status and location at any time. ------
 --- Overview of all books
-select * from borrowed inner JOIN book ON book.isbn = borrowed.isbn;
+SELECT * FROM Borrowed inner JOIN Book ON Book.ISBN = Borrowed.ISBN;
 
 --- giving a isbn return alle the people who has rented it
-select cprnummer, borrowed.isbn, count(*) from borrowed inner JOIN book ON book.isbn = borrowed.isbn GROUP BY cprnummer,borrowed.isbn ORDER BY cprnummer DESC;
+SELECT CPRNumber, Borrowed.ISBN, COUNT(*) FROM Borrowed INNER JOIN Book ON Book.ISBN = Borrowed.ISBN GROUP BY CPRNumber, Borrowed.ISBN ORDER BY CPRNumber DESC;
 
 
