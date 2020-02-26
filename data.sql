69e869ed64bb960c98d4d6de6532b8f303a77870
DunkRing <Chris31@live.dk>
2020-02-26 17:11:56 +0100

    works :)

--- data.sql
+++ data.sql
@@ -1,4 +1,3 @@
-
 -- Book data
 INSERT INTO Book VALUES (51232, 'Heste kan flyve', 'Jonas Pedersen', 'DinBogmand', '2019');
 INSERT INTO Book VALUES (51233, 'Kære fucking dabog', 'Henrik list', 'DinBogmand', '2012');
@@ -29,35 +28,35 @@ INSERT INTO Loaner VALUES (7777777772, 'Jyde', 'duestræde 91, 4000 Roskilde', '
 INSERT INTO Loaner VALUES (7777777773, 'Rasmus', 'allé gade 86, 4000 Roskilde', 'Guest');
 
 -- Orders 
-INSERT INTO Orders (ISBN, CPRNumber) VALUES(51232, 1234567891);
-INSERT INTO Orders (ISBN, CPRNumber) VALUES(51232, 1234567891);
-INSERT INTO Orders (ISBN, CPRNumber) VALUES(51233, 1234567841);
-INSERT INTO Orders (ISBN, CPRNumber) VALUES(51233, 1234567841);
-INSERT INTO Orders (ISBN, CPRNumber) VALUES(44444, 1234567861);
-INSERT INTO Orders (ISBN, CPRNumber) VALUES(44444, 1234567861);
-INSERT INTO Orders (ISBN, CPRNumber) VALUES(33333, 1111111114);
-INSERT INTO Orders (ISBN, CPRNumber) VALUES(11111, 7777777772);
-INSERT INTO Orders (ISBN, CPRNumber) VALUES(22222, 1111111112);
+INSERT INTO Orders (ISBN, CPRNumber) VALUES (51232, 1234567891);
+INSERT INTO Orders (ISBN, CPRNumber) VALUES (51232, 1234567891);
+INSERT INTO Orders (ISBN, CPRNumber) VALUES (51233, 1234567841);
+INSERT INTO Orders (ISBN, CPRNumber) VALUES (51233, 1234567841);
+INSERT INTO Orders (ISBN, CPRNumber) VALUES (44444, 1234567861);
+INSERT INTO Orders (ISBN, CPRNumber) VALUES (44444, 1234567861);
+INSERT INTO Orders (ISBN, CPRNumber) VALUES (33333, 1111111114);
+INSERT INTO Orders (ISBN, CPRNumber) VALUES (11111, 7777777772);
+INSERT INTO Orders (ISBN, CPRNumber) VALUES (22222, 1111111112);
 
 
 -- Status start point 
-INSERT INTO Status (ISBN, BookAmount, BookRented) VALUES(51232, 5,0);
-INSERT INTO Status (ISBN, BookAmount, BookRented) VALUES(51233, 5,0);
-INSERT INTO Status (ISBN, BookAmount, BookRented) VALUES(12345, 5,0);
-INSERT INTO Status (ISBN, BookAmount, BookRented) VALUES(55555, 5,0);
-INSERT INTO Status (ISBN, BookAmount, BookRented) VALUES(44444, 5,0);
-INSERT INTO Status (ISBN, BookAmount, BookRented) VALUES(33333, 5,0);
-INSERT INTO Status (ISBN, BookAmount, BookRented) VALUES(22222, 5,0);
-INSERT INTO Status (ISBN, BookAmount, BookRented) VALUES(11111, 5,0);
+INSERT INTO Status (ISBN, BookAmount, BookRented) VALUES (51232, 5,0);
+INSERT INTO Status (ISBN, BookAmount, BookRented) VALUES (51233, 5,0);
+INSERT INTO Status (ISBN, BookAmount, BookRented) VALUES (12345, 5,0);
+INSERT INTO Status (ISBN, BookAmount, BookRented) VALUES (55555, 5,0);
+INSERT INTO Status (ISBN, BookAmount, BookRented) VALUES (44444, 5,0);
+INSERT INTO Status (ISBN, BookAmount, BookRented) VALUES (33333, 5,0);
+INSERT INTO Status (ISBN, BookAmount, BookRented) VALUES (22222, 5,0);
+INSERT INTO Status (ISBN, BookAmount, BookRented) VALUES (11111, 5,0);
 
 -- Borrowed
-INSERT INTO Borrowed (ISBN, CPRNumber, LoanPeriodStart, LoanPeriodEnd)VALUES(51232, 1234567891, '2020-02-25', '2020-02-28');  --Student 
-INSERT INTO Borrowed (ISBN, CPRNumber, LoanPeriodStart, LoanPeriodEnd)VALUES(51232, 1234567891, '2020-02-25', '2020-02-28');  --Student 
-INSERT INTO Borrowed (ISBN, CPRNumber, LoanPeriodStart, LoanPeriodEnd)VALUES(51233, 1234567891, '2020-01-25', '2020-02-12');  --Student 
-INSERT INTO Borrowed (ISBN, CPRNumber, LoanPeriodStart, LoanPeriodEnd)VALUES(51232, 1234567831, '2020-02-25', '2020-02-28');  --Student 
-INSERT INTO Borrowed (ISBN, CPRNumber, LoanPeriodStart, LoanPeriodEnd)VALUES(55555, 1234567831, '2020-02-25', '2020-03-28');  --Student 
-INSERT INTO Borrowed (ISBN, CPRNumber, LoanPeriodStart, LoanPeriodEnd)VALUES(22222, 1234567851, '2020-01-25', '2020-02-12');  --Student 
-INSERT INTO Borrowed (ISBN, CPRNumber, LoanPeriodStart, LoanPeriodEnd)VALUES(22222, 1111111111, '2020-02-25', '2020-02-28');  --Teacher
-INSERT INTO Borrowed (ISBN, CPRNumber, LoanPeriodStart, LoanPeriodEnd)VALUES(51233, 1111111111, '2020-02-25', '2020-02-28');  --Teacher
-INSERT INTO Borrowed (ISBN, CPRNumber, LoanPeriodStart, LoanPeriodEnd)VALUES(44444, 7777777772, '2020-01-25', '2020-02-12');  --Guest
-INSERT INTO Borrowed (ISBN, CPRNumber, LoanPeriodStart, LoanPeriodEnd)VALUES(11111, 7777777777, '2020-02-25', '2020-02-28');  --Guest
+INSERT INTO Borrowed (ISBN, CPRNumber, LoanPeriodStart, LoanPeriodEnd) VALUES (51232, 1234567891, '2020-02-25', '2020-02-28');  --Student 
+INSERT INTO Borrowed (ISBN, CPRNumber, LoanPeriodStart, LoanPeriodEnd) VALUES (51232, 1234567891, '2020-02-25', '2020-02-28');  --Student 
+INSERT INTO Borrowed (ISBN, CPRNumber, LoanPeriodStart, LoanPeriodEnd) VALUES (51233, 1234567891, '2020-01-25', '2020-02-12');  --Student 
+INSERT INTO Borrowed (ISBN, CPRNumber, LoanPeriodStart, LoanPeriodEnd) VALUES (51232, 1234567831, '2020-02-25', '2020-02-28');  --Student 
+INSERT INTO Borrowed (ISBN, CPRNumber, LoanPeriodStart, LoanPeriodEnd) VALUES (55555, 1234567831, '2020-02-25', '2020-03-28');  --Student 
+INSERT INTO Borrowed (ISBN, CPRNumber, LoanPeriodStart, LoanPeriodEnd) VALUES (22222, 1234567851, '2020-01-25', '2020-02-12');  --Student 
+INSERT INTO Borrowed (ISBN, CPRNumber, LoanPeriodStart, LoanPeriodEnd) VALUES (22222, 1111111111, '2020-02-25', '2020-02-28');  --Teacher
+INSERT INTO Borrowed (ISBN, CPRNumber, LoanPeriodStart, LoanPeriodEnd) VALUES (51233, 1111111111, '2020-02-25', '2020-02-28');  --Teacher
+INSERT INTO Borrowed (ISBN, CPRNumber, LoanPeriodStart, LoanPeriodEnd) VALUES (44444, 7777777772, '2020-01-25', '2020-02-12');  --Guest
+INSERT INTO Borrowed (ISBN, CPRNumber, LoanPeriodStart, LoanPeriodEnd) VALUES (11111, 7777777777, '2020-02-25', '2020-02-28');  --Guest
